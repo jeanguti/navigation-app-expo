7a41cbdad8503bc22fb72efea9e4b305700b65e7
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-05-11 21:42:03 -0500

    feat: add custom Work Sans font family support and implement root layout with index view

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl font-bold text-blue-500">App</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className="text-5xl" style={{ fontFamily: "WorkSans-Black" }}>
+          Hola Mundo
+        </Text>
+        <Text className="text-4xl font-work-black">Hola Mundo</Text>
+        <Text className="text-3xl font-work-medium">Hola Mundo</Text>
+        <Text className="text-2xl font-work-light">Hola Mundo</Text>
+        <Text className="text-xl">Hola Mundo</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
